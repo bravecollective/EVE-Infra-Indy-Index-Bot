c969c39678e9980aaeea851dd9f8bad3c1df7858
Tian Khamez <tkhamez@gmail.com>
2023-11-15 18:41:55 +0100

    Fix indentation/remove zero-width space. Fix some style issues.

--- index_checker.py
+++ index_checker.py
@@ -1,4 +1,3 @@
-import operator
 import os
 import requests
 import json
@@ -7,31 +6,34 @@ import json
 #
 # Date: July 5, 2023
 #
-# The purpose of this program is to present a list of cost indexes for Manufacturing and Reactions for an alliance (or alliances) in EVE Online
+# The purpose of this program is to present a list of cost indexes for Manufacturing and Reactions for an alliance
+# (or alliances) in EVE Online
 # The data can then be sent to Discord or Slack
 #
-# Set it up on a CRON job or task scheduler to cycle regularly, and it'll keep you up to date on the cost indexes in your systems!
+# Set it up on a CRON job or task scheduler to cycle regularly, and it'll keep you up to date on the cost indexes
+# in your systems!
 
 
 # Formats indexes for sake of readability. Converts decimals into percentages.
-def indexFormatter (index):
+def indexFormatter(index):
     return f"{index:.2%}"
 
-def parseIndicesBySystemList (index_response, alliance_systems):
-    indices_list = [[],[]]
-    #Reads through all indexes returned by ESI
+
+def parseIndicesBySystemList(index_response, alliance_systems):
+    indices_list = [[], []]
+    # Reads through all indexes returned by ESI
 
     for cost_index in index_response:
-        #Then checks the list of system IDs in the alliance systems list, to see if it's on the list
+        # Then checks the list of system IDs in the alliance systems list, to see if it's on the list
         for system in alliance_systems:
             # If the systems match (cost index pertains to a system on the alliance_systems list
             if cost_index['solar_system_id'] == system['id']:
 
                 if cost_index['cost_indices'][0]['cost_index'] > configuration['display_threshold']:
-                    indices_list[0].append([system['name'],cost_index['cost_indices'][0]['cost_index']])
+                    indices_list[0].append([system['name'], cost_index['cost_indices'][0]['cost_index']])
 
                 if cost_index['cost_indices'][5]['cost_index'] > configuration['display_threshold']:
-                    indices_list[1].append([system['name'],cost_index['cost_indices'][5]['cost_index']])
+                    indices_list[1].append([system['name'], cost_index['cost_indices'][5]['cost_index']])
 
     # sorts the data
     indices_list[0].sort(key=lambda x: x[1])
@@ -42,25 +44,26 @@ def parseIndicesBySystemList (index_response, alliance_systems):
 
     return indices_list
 
-def buildOutputString (indices_list):
+
+def buildOutputString(indices_list):
     outputString = "Manufacturing Cost Index Report:\n```\n"
-​
+
     for system in indices_list[0]:
         system[1] = indexFormatter(system[1])
-​
         outputString += ('{0:7} {1:>8}'.format(*system)) + "\n"
-​
+
     outputString += "```\nReaction Cost Index Report:\n```\n"
-​
+
     for system in indices_list[1]:
         system[1] = indexFormatter(system[1])
-​
         outputString += ('{0:7} {1:>8}'.format(*system)) + "\n"
+
     outputString += "```"
-​
+
     return outputString
 
-def filterByRegions (alliance_systems):
+
+def filterByRegions(alliance_systems):
     inRegionFilteredList = []
     resolve_region_id_url = 'https://esi.evetech.net/latest/universe/regions/'
     resolve_constellation_id_url = 'https://esi.evetech.net/latest/universe/constellations/'
@@ -76,19 +79,22 @@ def filterByRegions (alliance_systems):
 
     return inRegionFilteredList
 
-def postSlackWebhook (content):
-    #Pulls the slack URL from the ENV Variable
+
+def postSlackWebhook(content):
+    # Pulls the slack URL from the ENV Variable
     slack_url = os.environ["INDY_BOT_SLACK_WEBHOOK_URL"]
 
-    #Sends it in the proper format, to the slack webhook
+    # Sends it in the proper format, to the slack webhook
     requests.post(slack_url, data=json.dumps({'text': content}), headers={'Content-Type': 'application/json'})
 
-def postDiscordWebhook (content):
-    #Pulls the discord webhook URL from the ENV variable
+
+def postDiscordWebhook(content):
+    # Pulls the discord webhook URL from the ENV variable
     discord_url = os.environ['INDY_BOT_DISCORD_WEBHOOK_URL']
 
-    #Sends it in the proper format, to the discord webhook
+    # Sends it in the proper format, to the discord webhook
     requests.post(discord_url, data=json.dumps({'content': content}), headers={'Content-Type': 'application/json'})
+
 
 def GetIndices(alliance_IDs):
     sov_url = 'https://esi.evetech.net/latest/sovereignty/structures/'
@@ -103,30 +109,33 @@ def GetIndices(alliance_IDs):
             alliance_systems.append(system['solar_system_id'])
 
     resolve_system_name_url = 'https://esi.evetech.net/latest/universe/names/'
-    alliance_systems = requests.post(resolve_system_name_url, headers={'Accept': 'application/json','Content-Type': 'application/json','Cache-Control': 'no-cache'}, json=alliance_systems).json()
+    alliance_systems = requests.post(resolve_system_name_url,
+                                     headers={'Accept': 'application/json', 'Content-Type': 'application/json',
+                                              'Cache-Control': 'no-cache'},
+                                     json=alliance_systems).json()
 
     alliance_systems = filterByRegions(alliance_systems)
 
     index_response = requests.get(indices_url).json()
 
-
     system_indices_list = parseIndicesBySystemList(index_response, alliance_systems)
 
     output_string = buildOutputString(system_indices_list)
 
-    #if the config file is true for slack
+    # if the config file is true for slack
     if configuration['webhooks']['slack']:
         postSlackWebhook(output_string)
 
-    #if the config file is true for discord
+    # if the config file is true for discord
     if configuration['webhooks']['discord']:
         postDiscordWebhook(output_string)
 
-#opens config file
+
+# opens config file
 configFile = open("./config.json")
 
-#returns JSON object as a dictionary
+# returns JSON object as a dictionary
 configuration = json.load(configFile)
 
-#Calls the function to fetch, and send, the indexes
+# Calls the function to fetch, and send, the indexes
 GetIndices(configuration["alliance_IDs"])
